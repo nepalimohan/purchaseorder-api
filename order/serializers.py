4f3d1252e2520a431f82c7367bfd62f1316c0c6f
nepalimohan <nepalimohan@outlook.com>
2024-01-28 13:15:38 +0545

    order create apiview

--- order/serializers.py
+++ order/serializers.py
@@ -12,4 +12,7 @@ class PurchaseOrderItemSerializer(serializers.ModelSerializer):
 class PurchaseOrderSerializer(serializers.ModelSerializer):
     items_read = PurchaseOrderItemSerializer(many=True, read_only=True)
     items_write = PurchaseOrderItemSerializer(many=True, write_only=True)
-    exclude = ['grn_status', 'invoice_status', 'english_cancel_date', 'nepali_cancel_date', 'english_approve_date', 'nepali_approve_date', 'submit_for_approval_nepali_date', 'submit_for_approval_english_date']
+
+    class Meta:
+        model = models.PurchaseOrders
+        exclude = ['grn_status', 'invoice_status', 'english_cancel_date', 'nepali_cancel_date', 'english_approve_date', 'nepali_approve_date', 'submit_for_approval_nepali_date', 'submit_for_approval_english_date']
--- order/views.py
+++ order/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -12,3 +13,13 @@ class PurchaseOrderListAPIView(APIView):
         queryset = models.PurchaseOrders.objects.all()
         serializer = serializers.PurchaseOrderSerializer(queryset, many=True)
         return Response(serializer.data)
+    
+class PurchaseOrderCreateAPIView(APIView):
+    def post(self, request):
+        serializer = serializers.PurchaseOrderSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            print(serializer.data)
+            # serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
